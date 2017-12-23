ac870b1d149a87f2da05ce0c6651ce81c4bbf052
Salvador <salvav1@uci.edu>
2017-12-22 22:49:33 -0800

    INDEX.HTML: I got the functionality now I need to make it look stylish

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------
+//
+// Variables needed
+//
+// ------------------------------------------------------
 var quotes = [
-  {"id": 0, "color": "purple", mewtwo: "I see now that the circumstances of one's birth are irrelevant. It is what you do with the gift of life that determines who you are."},
-  {"id": 1, "color": "green", khanacademy: "Failing is just another word for growing and you keep going. This is learning."},
-  {"id": 2, "color": "blue", oak: "There is a time and place for everything, but not now!"}
+  {"id": 0, "color": "#B149DE", mewtwo: "I see now that the circumstances of one's birth are irrelevant. It is what you do with the gift of life that determines who you are.", bio: "Mewtwo, Pokemon The First Movie: Mewtwo Strikes Back"},
+  {"id": 1, "color": "#08D42A", "Khan Academy": "Failing is just another word for growing and you keep going. This is learning.", bio: "Khan Academy, providing a world-class education for anyone, anywhere"},
+  {"id": 2, "color": "#6C8771", "Professor Oak": "There is a time and place for everything, but not now!", bio: "Professor Oak, since Pokemon Red and Green"}
 ];
 var html = "";
 var count = 0;
@@ -10,82 +25,90 @@ var twitter_text_array = [];
 var query_result = "";
 
 
+// ------------------------------------------------------
+//
+// query_twitter_text() -> this function does the URL encoding
+// and gives every space found in the quote a %20.
+//
+// ------------------------------------------------------
 function query_twitter_text()
 {
   query_result = twitter_text.split(" ").join("%20");
-  // for (var i = 0; i < twitter_text_array.length; i++)
-  // {
-  //   console.log(twitter_text_array[i]);
-  // }
-  // for (var word in twitter_text)
-  // {
-  //     console.log(twitter_text[word]);
-  // }
   return query_result;
 }
 
 
+// ------------------------------------------------------
+//
+// twitter_relocation() -> this function creates the
+// href link needed when the users click the twitter button
+//
+// ------------------------------------------------------
 function twitter_relocation()
 {
     location.href = "https://twitter.com/intent/tweet?text=" + query_twitter_text();
 }
 
 
-
-
+// ------------------------------------------------------
+//
+// This function is activated when the user clicks the
+// new quote button
+//
+// ------------------------------------------------------
 $(document).ready(function() {
   $("#new_quote").on("click", function() {
     $(".quote").hide();
+    var author_list = ["mewtwo", "Khan Academy", "Professor Oak"];
+
     for (var key in quotes) {
 
-      if (count === 0)
-      {
-        twitter_text = "";
-        html += "<div class='quote'>";
-        html += "<h5>" + quotes[0]["mewtwo"] + "</h5>";
-        html += "<p> -" + "Mewtwo" + "</p>";
-        $(".quote").html(quotes[key]).fadeIn(1000);
-        $(".quote").html(html);
-        html = "";
-        twitter_text += quotes[0]["mewtwo"] + " Mewtwo, Pokemon The First Movie: Mewtwo Strikes Back";
-        query_twitter_text();
-        break;
-      }
-      else if (count === 1)
-      {
-        twitter_text = "";
-        html += "<div class='quote'>";
-        html += "<h5>" + quotes[1]["khanacademy"] + "</h5>";
-        html += "<p> -" + "Khan Academy" + "</p>";
-        $(".quote").html(quotes[key]).fadeIn(1000);
-        $(".quote").html(html);
-        html = "";
-        twitter_text += quotes[1]["khanacademy"] + " Khan Academy, providing a world-class education for anyone, anywhere";
-        query_twitter_text();
-        break;
-      }
-      else if (count === 2)
-      {
-        twitter_text = "";
-        html += "<div class='quote'>";
-        html += "<h5>" + quotes[2]["oak"] + "</h5>";
-        html += "<p> -" + "Professor Oak" + "</p>";
-        $(".quote").html(quotes[key]).fadeIn(1000);
-        $(".quote").html(html);
-        html = "";
-        twitter_text += quotes[2]["oak"] + " Professor Oak, since Pokemon Red and Green";
-        query_twitter_text();
-        break;
-      }
+      // Variables neededd
+      // ------------------------------------------------------
+      twitter_text = "";
+      var quote = quotes[count][author_list[count]];
+      var capitalize_author = author_list[count][0].toUpperCase() + author_list[count].substring(1);
+      var background_color = quotes[count]["color"];
+      var brief_bio = quotes[count]["bio"];
+
+      // This creates the style sheet needed to change the
+      // background_color
+      // ------------------------------------------------------
+      var sheet = document.createElement('style');
+      sheet.innerHTML = "body {background-color: " + background_color +"}";
+      document.body.append(sheet);
+
+
+      // Changing the html to have background_color
+      // ------------------------------------------------------
+      html += "<div class='text-center quote' style='color:" + background_color + ";'>";
+      html += "<h5>" + quote + "</h5>";
+
+
+      // Changing the html to have the author with the first
+      // letter capitalize
+      // ------------------------------------------------------
+      html += "<p> -" + capitalize_author + "</p>";
+
+
+      // This creates output the quotes and author of quote
+      // to the user
+      // ------------------------------------------------------
+      $(".quote").html(quotes[key]).fadeIn(1000);
+      $(".quote").html(html);
+      html = "";
+
+      // This creates the twitter text needed when the user wants
+      // to share their quote on Twitter
+      // ------------------------------------------------------
+      twitter_text += quote + " " + brief_bio;
+      query_twitter_text();
+      break;
     }
+
     count++;
     if (count >= 3) {
       count = 0;
     }
   });
-
-  // $(".twitter-button").on("click", function() {
-  //
-  // }
-
 });
